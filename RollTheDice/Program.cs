229f0403ad1c04687bd0af067c3db417997ed975
Nicola TEB <nic00la.kaleta@gmail.com>
2025-02-13 11:10:11 +0100

    dziala

--- RollTheDice/Program.cs
+++ RollTheDice/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace RollTheDice
+{
+    namespace GraKosci
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                List<int> rzuty = new List<int>();
+                bool repeat = true;
+                Random rnd = new Random();
+                int count = 0;
+                int suma = 0;
+                while (repeat)
+                {
+                    Console.WriteLine("Ile kostek chcesz rzucic? (3 - 10)");
+                    count = Convert.ToInt32(Console.ReadLine());
+                    if (count >= 3 && count <= 10)
+                    {
+                        rzuty.Clear();
+                        suma = 0;
+                        for (int i = 0; i < count; i++)
+                        {
+                            int result = rnd.Next(1, 7);
+                            rzuty.Add(result);
+
+                            Console.WriteLine($"Kostka {i + 1}: {rzuty[i]}");
+                        }
+
+                        /*Sprawdza czy dany wynik się już powtórzył. Jezeli tak to dodaje go do siebie */
+                        for (int i = 0; i < rzuty.Count; i++)
+                        {
+                            bool isMultiply = false;
+                            for (int y = 0; y < rzuty.Count; y++)
+                            {
+                                if (rzuty[i] == rzuty[y] && i != y)
+                                {
+                                    isMultiply = true;
+
+                                }
+                            }
+
+                            if (isMultiply)
+                            {
+                                suma += rzuty[i];
+                            }
+                        }
+
+                        Console.WriteLine($"Liczba uzyskanych punktów: {suma}");
+                        Console.WriteLine("Jeszcze raz? (t/n)");
+                        bool odp = Console.ReadLine() == "t" ? true : false;
+
+                        if (!odp)
+                        {
+                            repeat = false;
+                        }
+
+                    }
+                }
+
+
+            }
+        }
+    }
+}
